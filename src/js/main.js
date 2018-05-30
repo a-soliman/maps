074c00aea5dc7b0f3c50cf6aa7cd0ae6eb4dfa88
a-soliman <ahmed.soliman@programmer.net>
2018-05-29 20:25:11 -0700

    adding a marker

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,14 @@
 var map;
     function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: {lat: 40.7413549, lng: -73.9980244},
+        zoom: 13
+        });
+
+        var tribeca = {lat: 40.719526, lng: -74.0089934};
+        var marker = new google.maps.Marker({
+            position: tribeca,
+            map:map,
+            title: 'First Marker!'
         });
     }
